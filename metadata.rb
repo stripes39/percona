537b55c540969f7113b86caa89821897ce0ba160
Patrick Connolly <patrick@myplanetdigital.com>
2012-04-30 22:53:00 -0400

    Initial skeleton commit.

---
+++ metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Myplanet Digital, Inc."
+maintainer_email "devops@myplanetdigital.com"
+license          "MIT"
+description      "Installs/Configures percona"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
